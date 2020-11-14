c2e776f4cdcdc1486063b5aa230934750f6b36d7
anusikh <anusikh2001@gmail.com>
2020-11-14 16:47:05 +0530

    6th commit

---
+++ GFG/staircase.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int Stairs(int n)
+{
+	int res[n+1];
+	res[0]=1;
+	res[1]=1;
+	res[2]=2;
+	for(int i=3;i<=n;i++)
+	{
+		res[i] = res[i-1]+res[i-2]+res[i-3];
+	}
+
+	return res[n];
+}
+
+int main()
+{
+	cout<<Stairs(4);
+	return 0;
+}
