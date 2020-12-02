258eabe1712516fe02d5a6a542948e4eba22a2ac
anusikh <anusikh2001@gmail.com>
2020-12-02 23:28:01 +0530

    7th commit

--- Graphs/DFS.cpp
+++ Graphs/DFS.cpp
@@ -61,22 +61,25 @@ void Graph::DFS(int s)
 	}
 
 	DFS_Util(s, visited);
-
 }
 
 
 int main()
 {
-	Graph g(4);
+	Graph g(10);
 	g.add_edge(0, 1);
 	g.add_edge(0, 2);
-	g.add_edge(1, 2);
-	g.add_edge(2, 0);
+	g.add_edge(1, 4);
+	g.add_edge(2, 4);
 	g.add_edge(2, 3);
-	g.add_edge(3, 3);
+	g.add_edge(4, 3);
+	g.add_edge(4, 0);
+	g.add_edge(4, 5);
+	g.add_edge(5, 6);
+	g.add_edge(6, 3);
 
 	//g.print();
 
-	g.DFS(2);
+	g.DFS(4);
 	return 0;
 }
