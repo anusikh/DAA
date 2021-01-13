e7b52668bd263c6cfef155b4b0950f860e9f5237
anusikh <anusikh2001@gmail.com>
2021-01-13 16:51:57 +0530

    16th commit

---
+++ Sorting/bubblesort.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+using namespace std;
+
+void bubblesort(int arr[], int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(arr[j]>arr[j+1])
+                swap(arr[j],arr[j+1]);
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {1,4,3,5,7};
+    int n =sizeof(arr)/sizeof(arr[0]);
+    
+    bubblesort(arr,n);
+    
+    for(int i=0;i<n;i++)
+        cout<<arr[i]<<" ";
+        
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
